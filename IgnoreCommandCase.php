c98ae33b25dc8a525c1e2ea02efc9b92cc88ec82
PresentKim <qjrmckd@gmail.com>
2018-01-17 08:26:59 +0900

    UPADTE LICENSE

--- IgnoreCommandCase.php
+++ IgnoreCommandCase.php
@@ -1,6 +1,15 @@
 <?php
 
 /**
+ * IgnoreCommandCase plugin for PocketMine-MP
+ * Copyright (C) 2018 PresentKim <https://github.com/PMMPPlugin>
+ *
+ * The AGPL license differs from the other GNU licenses in that it was built for network software.
+ * You can distribute modified versions if you keep track of the changes and the date you made them.
+ * As per usual with GNU licenses, you must license derivatives under AGPL.
+ * It provides the same restrictions and freedoms as the GPLv3 but with an additional clause which makes it so that source code must be distributed along with web publication.
+ * Since web sites and services are never distributed in the traditional sense, the AGPL is the GPL of the web.
+ *
  * @name        IgnoreCommandCase
  * @main        presentkim\singleton\IgnoreCommandCase
  * @version     1.0.0
